7a3cb2ac23e44cb1e7c212065abbf8e79d75cd00
KaiLee2868 <103954255+KaiLee2868@users.noreply.github.com>
2022-08-24 13:10:18 -0400

    Add files via upload

---
+++ LibraryApp/LibraryApplication/Controllers/HomeController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibraryApplication.Context;
+using LibraryApplication.Models;
+
+namespace LibraryApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LibraryDb db = new LibraryDb();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            int maxListCount = 3;
+            int pageNum = 1;
+            string keyword = Request.QueryString["keyword"] ?? string.Empty;
+            string searchKind = Request.QueryString["searchKind"] ?? string.Empty;
+            int totalCount = 0;
+            if (Request.QueryString["page"] != null)
+                pageNum = Convert.ToInt32(Request.QueryString["page"]);
+            var books = new List<Book>();
+
+            if(string.IsNullOrWhiteSpace(keyword))
+            {
+                books = db.Books.ToList();
+                totalCount = db.Books.Count();
+            }
+            else
+            {
+                switch(searchKind)
+                {
+                    case "Title":
+                        books = db.Books.Where(x => x.Title.Contains(keyword)).ToList();
+                        totalCount = books.Count();
+                        break;
+                    case "Writer":
+                        books = db.Books.Where(x => x.Writer.Contains(keyword)).ToList();
+                        totalCount = books.Count();
+                        break;
+                    case "Publisher":
+                        books = db.Books.Where(x => x.Pulisher.Contains(keyword)).ToList();
+                        totalCount = books.Count();
+                        break;
+                }               
+                
+            }
+            books = books.OrderBy(x => x.BookId).Skip((pageNum - 1) * maxListCount).Take(maxListCount).ToList();
+
+            ViewBag.Page = pageNum;
+            ViewBag.TotalCount = totalCount;
+            ViewBag.MaxListCount = maxListCount;
+            ViewBag.Searchkind = searchKind;
+            ViewBag.Keyword = keyword;
+            return View(books);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookId,Title,Writer,Summary,Pulisher,Published_date")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookId,Title,Writer,Summary,Pulisher,Published_date")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
